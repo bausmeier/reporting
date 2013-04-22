4bb609fb898a65b08771669052331c93d4622583
Brett Ausmeier <brett.ausmeier@bsg.co.za>
2013-04-22 13:59:44 +0200

    Total visits

--- auth.js
+++ auth.js
@@ -3,13 +3,11 @@ var apiKey = 'AIzaSyBukblQ5fz05neRK5B02AiwvGG6MaqmQ90';
 var scopes = 'https://www.googleapis.com/auth/analytics.readonly';
 
 var handleClientLoad = function() {
-  console.debug('handleClientLoad');
   gapi.client.setApiKey(apiKey);
   window.setTimeout(checkAuth, 1);
 }
 
 var checkAuth = function() {
-  console.debug('checkAuth');
   var data = {
     client_id: clientId,
     scope: scopes,
@@ -19,9 +17,7 @@ var checkAuth = function() {
 }
 
 var handleAuthResult = function(authResult) {
-  console.debug('handleAuthResult');
   if (authResult) {
-    console.debug('Auth success');
     gapi.client.load('analytics', 'v3', handleAuthorized);
   } else {
     console.error('Auth failed');
@@ -29,5 +25,15 @@ var handleAuthResult = function(authResult) {
 }
 
 var handleAuthorized = function() {
-  alert('Client loaded!');
+  var query = {
+    'ids': 'ga:68220841',
+    'start-date': '2013-04-17',
+    'end-date': '2013-04-22',
+    'metrics': 'ga:visits'
+  }
+  gapi.client.analytics.data.ga.get(query).execute(handleResults);
+}
+
+var handleResults = function(results) {
+  alert('Total visits: '+results.rows[0][0]);
 }
