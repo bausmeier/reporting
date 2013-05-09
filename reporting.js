f8df8dfb06c3f031cc9e9447e9ff1b32607c3eac
Brett Ausmeier <brett.ausmeier@bsg.co.za>
2013-05-09 14:33:35 +0200

    Add unique events

--- reporting.js
+++ reporting.js
@@ -1,4 +1,4 @@
-var clientId = '16305868429.apps.googleusercontent.com';
+var clientId = '16305868429-70lrbni5o6mi87bifvps8unt04fbl0ak.apps.googleusercontent.com';
 var apiKey = 'AIzaSyBukblQ5fz05neRK5B02AiwvGG6MaqmQ90';
 var scopes = 'https://www.googleapis.com/auth/analytics.readonly';
 
@@ -75,7 +75,7 @@ var queryEvents = function(tenant) {
     'ids': 'ga:68220841',
     'start-date': '2013-04-01',
     'end-date': '2013-05-08',
-    'metrics': 'ga:totalEvents',
+    'metrics': 'ga:totalEvents,ga:uniqueEvents',
     'dimensions': 'ga:eventCategory,ga:eventAction',
     'filters': 'ga:customVarValue1=~' + tenant
   }
@@ -92,8 +92,8 @@ var handlePageResults = function(results) {
 
 var handleEventResults = function(results) {
   var content = $('#events');
-  content.append('<tr><th>Event category</th><th>Event action</th><th>Count</th></tr>');
+  content.append('<tr><th>Event category</th><th>Event action</th><th>Count</th><th>Unique</th></tr>');
   results.rows.forEach(function(row) {
-    content.append('<tr><td>'+row[0]+'</td><td>'+row[1]+'</td><td>'+row[2]+'</td></tr>');
+    content.append('<tr><td>'+row[0]+'</td><td>'+row[1]+'</td><td>'+row[2]+'</td><td>'+row[3]+'</td><</tr>');
   });
 }
